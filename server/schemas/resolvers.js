105ea2ca71db3c196364b95a8314134148e17ee4
EvrettG <skyrift@tpg.com.au>
2024-07-27 20:12:07 +1000

    Small errors fixed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,14 +5,14 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        getSingleUser: async (parent, { username }) => {
+        me: async (parent, { username }) => {
             return User.findOne({ username });
         },
     },
-    
+
     Mutation: {
 
-        createUser: async (parent, { username, email, password }) => {
+        addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
@@ -42,19 +42,19 @@ const resolvers = {
                     { _id: user._Id },
                     { $addToSet: { savedBooks: book } },
                     { new: true, runValidators: true }
-                );
+                ).populate('savedBooks');;
                 return updatedUser;
             } 
             throw AuthenticationError;
           },
 
-          deleteBook: async (parent, {user, params}, context) => {
+          removeBook: async (parent, {bookIdPara}, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: user._id },
-                    { $pull: { savedBooks: { bookId: params.bookId } } },
+                    { $pull: { savedBooks: { bookId: bookIdPara } } },
                     { new: true }
-                );
+                ).populate('savedBooks');;
                 return updatedUser;
             }
             throw AuthenticationError;
